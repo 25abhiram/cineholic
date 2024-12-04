a2bca8c35747ea97d6acf74a20a2feb5a002cd93
25abhiram <ravikumarabhiram7@gmail.com>
2024-12-04 22:11:04 +0530

    project created

---
+++ src/main/java/com/movie/cineholic/CineholicApplication.java
@@ -0,0 +1,13 @@
+package com.movie.cineholic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CineholicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CineholicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/movie/cineholic/CineholicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.movie.cineholic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CineholicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

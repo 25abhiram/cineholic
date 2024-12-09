dda2e30183552a1310499b4aff44749ffef58d5a
25abhiram <ravikumarabhiram7@gmail.com>
2024-12-10 02:10:00 +0530

    update code user entity

--- src/main/java/com/movie/cineholic/Controller/UserController.java
+++ src/main/java/com/movie/cineholic/Controller/UserController.java
@@ -1,10 +1,16 @@
 package com.movie.cineholic.Controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.movie.cineholic.Model.User;
+import com.movie.cineholic.Service.impl.UserServiceImpl;
 
 @RestController
 @RequestMapping("/api/v1/users")
@@ -13,8 +19,11 @@ public class UserController {
 // public String allMovies(){
 //     return "hello bro";
 // }
+@Autowired
+private UserServiceImpl userServiceImpl;
 @GetMapping
-public ResponseEntity<String> allUsers(){
-    return new ResponseEntity<String>("All Users",HttpStatus.OK);
+public ResponseEntity<List<User>> getAllUsers(){
+    return new ResponseEntity<List<User>>(userServiceImpl.allUsers(), HttpStatus.OK);
+
 }
 }
--- src/main/java/com/movie/cineholic/Service/impl/UserServiceImpl.java
+++ src/main/java/com/movie/cineholic/Service/impl/UserServiceImpl.java
@@ -1,5 +1,19 @@
 package com.movie.cineholic.Service.impl;
 
-public class UserServiceImpl {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.movie.cineholic.Model.User;
+import com.movie.cineholic.Repository.UserRepository;
+
+@Service
+public class UserServiceImpl {
+    @Autowired
+    private UserRepository userRepository;
+
+    public List<User> allUsers() {
+        return userRepository.findAll();
+    }
 }

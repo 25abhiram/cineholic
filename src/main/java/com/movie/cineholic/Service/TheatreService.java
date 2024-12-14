19bd2260d7883d15be4b1638ae91a24e9ea9a22a
25abhiram <ravikumarabhiram7@gmail.com>
2024-12-14 11:38:36 +0530

    TheatreServiceImpl class implemented

--- src/main/java/com/movie/cineholic/Service/TheatreService.java
+++ src/main/java/com/movie/cineholic/Service/TheatreService.java
@@ -1,5 +1,13 @@
 package com.movie.cineholic.Service;
 
-public interface TheatreService {
+import java.util.List;
 
+import com.movie.cineholic.Model.Theatre;
+
+public interface TheatreService {
+Theatre createTheatre(Theatre theatre);
+Theatre getTheatreById(String theatreId);
+List<Theatre> getAllTheatres();
+Theatre updateTheatre(String theatreId,Theatre theatre);
+void deleteTheatre(String theatreId);
 }
--- src/main/java/com/movie/cineholic/Service/impl/TheatreServiceImpl.java
+++ src/main/java/com/movie/cineholic/Service/impl/TheatreServiceImpl.java
@@ -1,5 +1,47 @@
 package com.movie.cineholic.Service.impl;
 
-public class TheatreServiceImpl {
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.movie.cineholic.Model.Theatre;
+import com.movie.cineholic.Repository.TheatreRepository;
+import com.movie.cineholic.Service.TheatreService;
+
+@Service
+public class TheatreServiceImpl implements TheatreService{
+@Autowired
+    private TheatreRepository theatreRepository;
+    @Override
+    public Theatre createTheatre(Theatre theatre) {
+        return theatreRepository.save(theatre);
+    }
+
+    @Override
+    public Theatre getTheatreById(String theatreId) {
+        Optional<Theatre> theatre=theatreRepository.findById(theatreId);
+        return theatre.orElse(null);
+    }
+
+    @Override
+    public List<Theatre> getAllTheatres() {
+        return theatreRepository.findAll();
+    }
+
+    @Override
+    public Theatre updateTheatre(String theatreId, Theatre theatre) {
+        if (theatreRepository.existsById(theatreId)) {
+            theatre.setTheatreId(theatreId);
+            return theatreRepository.save(theatre);            
+        }
+        return null;
+    }
+
+    @Override
+    public void deleteTheatre(String theatreId) {
+        theatreRepository.deleteById(theatreId);
+    }
 
 }

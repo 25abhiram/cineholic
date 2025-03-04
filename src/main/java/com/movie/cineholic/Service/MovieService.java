53861674984470f90ede8ef6021c340d4bb74b85
Nithusan01 <b.nithusan01@gmail.com>
2025-03-04 16:24:36 +0530

    update movie service

--- src/main/java/com/movie/cineholic/Service/MovieService.java
+++ src/main/java/com/movie/cineholic/Service/MovieService.java
@@ -10,7 +10,7 @@ public interface MovieService {
     Movie addMovie(Movie movie);
     Movie updateMovie(String movieId,Movie movie);
     void deleteMovie(String movieId);
-    Movie getMovieById(String movieId);
+    Optional<Movie> getMovieById(String movieId);
     List<Movie> getMoviesByGenres(String genres);
     List<Movie> getMoviesByTitle(String title);
     List<Movie> getMoviesWithRatingAbove(double averageRating);
